d36337333594ab133e87bb59962a07d89b359fa9
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-11-23 01:34:23 -0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.6.1/firebase-messaging.js");
 firebase.initializeApp({
-  apiKey: "Your apiKey",
-  authDomain: "Your authDomain",
-  projectId: "Your projectId",
-  storageBucket: "Your storageBucket",
-  messagingSenderId: "Your messagingSenderId",
-  appId: "Your appId",
-  measurementId: "Your measurementId",
+  apiKey: "AIzaSyD5wGV27bLLhGvncsEHCAJESnoGLYBTUhE",
+  authDomain: "edubest-a9dee.firebaseapp.com",
+  projectId: "edubest-a9dee",
+  storageBucket: "edubest-a9dee.firebasestorage.app",
+  messagingSenderId: "76598492826",
+  appId: "1:76598492826:web:ec10a0acd1cb8496386923",
+  measurementId: "G-LJWCTRFBPT"
 });
 const messaging = firebase.messaging();
